c3d366efdfb81c6ea1d0978136a7529b18e0b518
Kanan <43238830+KenanBoyukkishiyev1997@users.noreply.github.com>
2021-12-03 00:00:31 +0400

    done

---
+++ CmsA/wwwroot/js/lightbox.js
@@ -0,0 +1,8 @@
+﻿lightGallery(document.getElementById('animated-thumbnails'), {
+    thumbnail: true,
+    animateThumb: false,
+    zoomFromOrigin: false,
+    allowMediaOverlap: true,
+    toggleThumb: true,
+
+});
